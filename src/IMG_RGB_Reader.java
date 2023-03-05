cf01928fc2d38e261b08b9e656766df118823e1f
lethangomes <35245543+lethangomes@users.noreply.github.com>
2023-03-05 15:35:44 -0800

    make file chooser better

--- src/IMG_RGB_Reader.java
+++ src/IMG_RGB_Reader.java
@@ -22,6 +22,7 @@ import java.util.Scanner;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -35,7 +36,6 @@ public class IMG_RGB_Reader implements ActionListener, MouseListener, MouseMotio
 	JFrame frame = new JFrame();
 	
 	JTextField fileChooserField = new JTextField();
-	JButton fileChooserB = new JButton("Enter");
 	JButton compareBoxesB = new JButton("Compare Boxes");
 	JButton deleteBoxesB = new JButton("Delete Box");
 	JButton changeImageB = new JButton("Change Image");
@@ -69,6 +69,8 @@ public class IMG_RGB_Reader implements ActionListener, MouseListener, MouseMotio
 	int frameWidthOffset = 200;
 	int frameHeightOffset = 100;
 	
+	final JFileChooser fc = new JFileChooser();
+	
 	public static void main(String[] args) 
 	{
 		
@@ -77,16 +79,18 @@ public class IMG_RGB_Reader implements ActionListener, MouseListener, MouseMotio
 	
 	public IMG_RGB_Reader()
 	{
-		//creates window for user to enter image file name
-		fileChooser.setLayout(new BorderLayout());
-		fileChooser.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		fileChooser.add(fileChooserField);
-		fileChooser.add(fileChooserField, BorderLayout.CENTER);
-		fileChooserB.addActionListener(this);
-		fileChooser.add(fileChooserB, BorderLayout.EAST);
-		fileChooser.setSize(500, 100);
-		fileChooser.addWindowListener(this);
-		fileChooser.setVisible(true);
+		//opens file chooser to get image
+		int fileNum = fc.showOpenDialog(fileChooser);
+		if(fileNum == JFileChooser.APPROVE_OPTION)
+		{
+			File file = fc.getSelectedFile();
+			readFile(file);
+		}
+		else
+		{
+			frame.dispose();
+			System.exit(0);
+		}
 	}
 	
 	public void makeFrame()
@@ -246,50 +250,6 @@ public class IMG_RGB_Reader implements ActionListener, MouseListener, MouseMotio
 				deleteBoxesB.setBackground(Color.gray);
 			}
 		}
-		if(e.getSource().equals(fileChooserB)) //file chooser button pressed
-		{
-			//attempts to find file based on user input
-			File file = new File(fileChooserField.getText());
-			
-			//attempts to read file
-			try {
-				//sets image to the file specified by user, then creates window to display image
-				 image = ImageIO.read(file);
-				 double aspectRatio = (double)image.getWidth()/image.getHeight();
-				 
-				 //adjusts image if it's too wide
-				 if(image.getWidth() + frameWidthOffset > screenWidth)
-				 {
-					 image = resize(image, screenWidth - frameWidthOffset, (int)((screenWidth - frameWidthOffset) * (1/aspectRatio)));
-				 }
-				 
-				 //adjusts image if it's too tall
-				 if(image.getHeight() + frameHeightOffset > screenHeight)
-				 {
-					 image = resize(image, (int)((screenHeight - frameHeightOffset) * aspectRatio), screenHeight - frameHeightOffset);
-				 }
-				 
-				 panel.setImage(image);
-				 fileChooser.setVisible(false);
-				 if(state == NONE)
-				 {
-					 //creates frame if the frame hasn't been made yet
-					 makeFrame();
-					 fileChooser.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-				 }
-				 else if(state == CHANGING_IMAGE)
-				 {
-					 //updates frame size to match new image and clears all boxes
-					 panel.clearBoxes();
-					 frame.setSize(image.getWidth() + frameWidthOffset, image.getHeight() + frameHeightOffset);
-					 state = NONE;
-				 }
-			} catch (IOException ee) {
-				//if the file was not found, or is not readable notifies user.
-				ee.printStackTrace();
-				JOptionPane.showMessageDialog(frame, "File \"" + fileChooserField.getText()+ "\" not found");
-			}
-		}
 		if(e.getSource().equals(deleteBoxesB)) // delete box button pressed
 		{
 			//if button pressed in delete boxes state, resets state
@@ -309,13 +269,20 @@ public class IMG_RGB_Reader implements ActionListener, MouseListener, MouseMotio
 		if(e.getSource().equals(changeImageB)) //change image button pressed
 		{
 			//makes file chooser window visible
-			fileChooser.setVisible(true);
+			
 			state = CHANGING_IMAGE;
-		}
-		else if(!e.getSource().equals(fileChooserB)) //anything other than the change image button or the file chooser button pressed
-		{
-			//makes file choose window invisible
-			fileChooser.setVisible(false);
+			
+			//opens file chooser
+			int fileNum = fc.showOpenDialog(fileChooser);
+			if(fileNum == JFileChooser.APPROVE_OPTION)
+			{
+				File file = fc.getSelectedFile();
+				readFile(file);
+			}
+			else
+			{
+				state = NONE;
+			}
 		}
 		
 	}
@@ -413,4 +380,45 @@ public class IMG_RGB_Reader implements ActionListener, MouseListener, MouseMotio
 		
 	}  
 	
+	
+	public void readFile(File file)
+	{
+		try {
+			//sets image to the file specified by user, then creates window to display image
+			 image = ImageIO.read(file);
+			 double aspectRatio = (double)image.getWidth()/image.getHeight();
+			 
+			 //adjusts image if it's too wide
+			 if(image.getWidth() + frameWidthOffset > screenWidth)
+			 {
+				 image = resize(image, screenWidth - frameWidthOffset, (int)((screenWidth - frameWidthOffset) * (1/aspectRatio)));
+			 }
+			 
+			 //adjusts image if it's too tall
+			 if(image.getHeight() + frameHeightOffset > screenHeight)
+			 {
+				 image = resize(image, (int)((screenHeight - frameHeightOffset) * aspectRatio), screenHeight - frameHeightOffset);
+			 }
+			 
+			 panel.setImage(image);
+			 fileChooser.setVisible(false);
+			 if(state == NONE)
+			 {
+				 //creates frame if the frame hasn't been made yet
+				 makeFrame();
+				 fileChooser.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+			 }
+			 else if(state == CHANGING_IMAGE)
+			 {
+				 //updates frame size to match new image and clears all boxes
+				 panel.clearBoxes();
+				 frame.setSize(image.getWidth() + frameWidthOffset, image.getHeight() + frameHeightOffset);
+				 state = NONE;
+			 }
+		} catch (IOException ee) {
+			//if the file was not found, or is not readable notifies user.
+			ee.printStackTrace();
+			JOptionPane.showMessageDialog(frame, "File \"" + fileChooserField.getText()+ "\" not found");
+		}
+	}
 }
